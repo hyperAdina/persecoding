8e64f2db67e12fba9988600a0b05f946e00fdbe2
YAaKkaWa <125532768+hyperAdina@users.noreply.github.com>
2023-03-06 23:23:20 +0800

    Create pctc.py

---
+++ pctc.py
@@ -0,0 +1,35 @@
+#round 1 demo 1: 
+string1 = input()
+string2 = input()
+print(string1+" "+string2)
+
+#round 1 demo 2
+n = int(input())
+x = int(input())
+if n>x:
+    print(n-x)
+elif x>n:
+    print(x-n)
+    
+#round 1 demo 3
+string = input()
+n=int(30/len(string))+1
+string2=string*n
+print(string2)
+
+#round 1 demo 4
+numberChoice = int(input())
+if numberChoice>50:
+     print("yes dream big")
+else:
+     print("on the small side")
+   
+#round 1 demo 5
+n = int(input())
+k = input()
+x = int(input())
+if (k=="*"):
+    print(n*x)
+elif (k=="+"):
+    print(n+x)
+    
